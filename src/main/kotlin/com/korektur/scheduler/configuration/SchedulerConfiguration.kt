c5d7a4f46093032df9136d2dfdd9a52be3e6f842
Ruslan Akhundov <akhundov2@gmail.com>
2018-06-06 22:41:08 +0200

    migrated back to gradle

--- src/main/kotlin/com/korektur/scheduler/configuration/SchedulerConfiguration.kt
+++ src/main/kotlin/com/korektur/scheduler/configuration/SchedulerConfiguration.kt
@@ -1,5 +1,12 @@
 package com.korektur.scheduler.configuration
 
-class SchedulerConfiguration {
+class SchedulerConfiguration internal constructor()  {
+
+    companion object {
+        @JvmStatic
+        public val instance: SchedulerConfiguration by lazy { Holder.INSTANCE }
+    }
+
+    private object Holder { val INSTANCE = SchedulerConfigurationPropertiesParser().parse() }
 
 }
